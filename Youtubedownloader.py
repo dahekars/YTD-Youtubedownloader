d27c6c57fe7d0993f6032ea60f3acd3987affb9a
Shreeyash dahekar (Anthony) <36699222+dahekars@users.noreply.github.com>
2021-03-27 16:03:39 +0530

    removed KeyboardInterrupt error
    removed KeyboardInterrupt error to provide right comment and no error on Screen

--- Youtubedownloader.py
+++ Youtubedownloader.py
@@ -37,27 +37,29 @@ ipaddress_internal = soc.gethostbyname(hostname)
 externalip = systemid.popen('wget -qO- http://ipecho.net/plain ; echo').readlines(-1)[0].strip()
 
 print(f"hello {hostname},\nyour private ip is {ipaddress_internal}.\nand your public ip is {externalip}\nI hope you are a Great!!!")
-
-URL_INPUT = str(input("Kindly provide URL of you YOUTUBE Playlist/Video:\n>>"))
-res = input("RES == resolution\n>>") + "p"
-if len(URL_INPUT) == 0:
-    print("Sorry!! no Input received...")
-else:
-    path = path()
-    print(path)
-    confirmation = str(URL_INPUT.capitalize())#https://www.youtube.com/playlist?list=PLoicwUXm27GO54OtCCVJtUXJyD-4oXgxC
-    if confirmation.find("playlist") >= 0:
-        PLAYLIST_Order = pyd2(URL_INPUT)
-        count_of_video_urls = len(PLAYLIST_Order.video_urls)
-        if count_of_video_urls !=0:
-            for video_url in PLAYLIST_Order:
-                link = video_url
-                print(video_url)
+try:
+    URL_INPUT = str(input("Kindly provide URL of you YOUTUBE Playlist/Video:\n>>"))
+    res = input("RES == resolution\n>>") + "p"
+    if len(URL_INPUT) == 0:
+        print("Sorry!! no Input received...")
+    else:
+        path = path()
+        print(path)
+        confirmation = str(URL_INPUT.capitalize())#https://www.youtube.com/playlist?list=PLoicwUXm27GO54OtCCVJtUXJyD-4oXgxC
+        if confirmation.find("playlist") >= 0:
+            PLAYLIST_Order = pyd2(URL_INPUT)
+            count_of_video_urls = len(PLAYLIST_Order.video_urls)
+            if count_of_video_urls !=0:
+                for video_url in PLAYLIST_Order:
+                    link = video_url
+                    print(video_url)
+                time.sleep(3)
+                print(f"Number of Video URL in Playlist: {count_of_video_urls}")
+                for url in PLAYLIST_Order:
+                    video_downloader(url)
+        elif confirmation.find("playlist") <= 0:#https://www.youtube.com/watch?v=T8g2pcgRiLI
             time.sleep(3)
-            print(f"Number of Video URL in Playlist: {count_of_video_urls}")
-            for url in PLAYLIST_Order:
-                video_downloader(url)
-    elif confirmation.find("playlist") <= 0:#https://www.youtube.com/watch?v=T8g2pcgRiLI
-        time.sleep(3)
-        url = URL_INPUT
-        video_downloader(url)
+            url = URL_INPUT
+            video_downloader(url)
+except KeyboardInterrupt as e:
+    print("\nProcess cancelled by user\n")
